f718568818f38a10a3aa9e959a83c35533f7e144
blgutie1 <blgutie1@github.com>
2022-03-23 18:24:36 -0700

    Fixed select statement (ResultSet was leaving out results)

--- src/SQL/Main.java
+++ src/SQL/Main.java
@@ -9,6 +9,7 @@ public class Main{
     	Menu menu = new Menu();
     	Scanner input = new Scanner(System.in);
     	String name = "";
+    	String size = "";
     	int id = 0;
     	while(true) {
     		menu.MenuPrint();
@@ -16,32 +17,62 @@ public class Main{
     		int option = input.nextInt();
 			switch(option) {
 				case 1:
-					data.Select();
+					System.out.println("What name?(Type All to view all)");
+					input = new Scanner(System.in);
+					name = input.nextLine();
+					if(name.equals("All") || name.equals("all")) {
+						data.Select(name, id);
+					}else {
+						System.out.println("What ID?(If unknown, type 0)");
+						id = input.nextInt();
+						data.Select(name, id);
+					}
 					continue;
 				case 2:
 					System.out.println("What name? ");
 					input = new Scanner(System.in);
-					name = input.next();
+					name = input.nextLine();
 					System.out.println("What Price? ");
 					input = new Scanner(System.in);
 					float price = input.nextFloat();
-					System.out.println("What Size (Small, Large, N/A)?  ");
-					String size = input.next();
+					while(true) {
+						System.out.println("What Size (Small, Large, N/A)?  ");
+						size = input.next();
+						switch(size) {
+							case "small":
+							case "Small":
+								size = "Small";
+								break;
+							case "Large":
+							case "large":
+								size = "Large";
+								break;
+							case "N/A":
+							case "n/a":
+								size = "N/A";
+								break;
+							default:
+								continue;
+						}
+						break;
+					}
 			    	data.Insert(name, price, size);
 			    	continue;
 				case 3:
 					System.out.println("What name? ");
 					input = new Scanner(System.in);
-					name = input.next();
+					name = input.nextLine();
 					System.out.println("What ID? ");
 					input = new Scanner(System.in);
 					id = input.nextInt();
-					data.Delete(name, id);
+					data.Delete(name, id, input);
 					continue;
 				case 4:
 					System.out.println("Goodbye!");
 					input.close();
 					break;
+				default:
+					continue;
 		        
 			}
 			break;
--- src/SQL/SQLDatabaseConnection.java
+++ src/SQL/SQLDatabaseConnection.java
@@ -4,6 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 import java.sql.PreparedStatement;
 
 public class SQLDatabaseConnection {
@@ -16,23 +17,41 @@ public class SQLDatabaseConnection {
             + "loginTimeout=30;";
     // Connect to your database.
     // Replace server name, username, and password with your credentials
-    public void Select() {
+    public boolean Select(String name, int ID) {
         ResultSet resultSet = null;
+        //Statement parameters are due to ResultSet objects incrementing when calling next() on ResultSet objects.
         try (Connection connection = DriverManager.getConnection(connectionUrl);
-                Statement statement = connection.createStatement();) {
+                Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);) {
 
             // Create and execute a SELECT SQL statement.
-            String selectSql = "SELECT * from dbo.Menu_Items";
+            String selectSql = "SELECT * from dbo.Menu_Items WHERE " + "Name = " + "'" + name + "' " + "AND " + "ID = " + ID + ";";
+            if(name.equals("All") || name.equals("all")) {
+            	selectSql = "SELECT * from dbo.Menu_Items;";
+            }else if(ID == 0) {
+            	selectSql = "SELECT * FROM dbo.Menu_Items WHERE Name = " + "'" + name + "'" + ";";
+            }
             resultSet = statement.executeQuery(selectSql);
 
             // Print results from select statement
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString(4) + " " + resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3));
+            if(resultSet.next()) {
+            	resultSet.beforeFirst();	//ResultSet.TYPE_SCROLL_INSENSITIVE allows for this statement to work.
+    	        while (resultSet.next()) {
+    	            System.out.print(resultSet.getString(4) + " " + resultSet.getString(1) + " ");
+    	            System.out.printf("%.2f", Float.valueOf(resultSet.getString(2)).floatValue());
+    	            System.out.println(" " + resultSet.getString(3));
+    	        }
+            }else {
+            	System.out.println("No entries found.\n");
+            	return false;
             }
+
+	        System.out.println();
+	        return true;
         }
         catch (SQLException e) {
             e.printStackTrace();
         }
+        return false;
     }
     public void Insert(String name, float price, String size) {
         String insertSql = "INSERT INTO [dbo].[Menu_Items] (Name, Price, Size, ID) VALUES " 
@@ -48,25 +67,47 @@ public class SQLDatabaseConnection {
                 PreparedStatement prepsInsertMenuItem = connection.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);) {
 
         	prepsInsertMenuItem.execute();
+        	System.out.println("Added Successfully.");
         }
         // Handle any errors that may have occurred.
         catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public void Delete(String name, int ID) {
-    	 String deleteSql = "DELETE FROM dbo.Menu_Items WHERE " + "ID = " + ID + " " + "AND " + "NAME = " + "'" + name + "'";
-
+    public void Delete(String name, int ID, Scanner input) {
          try (Connection connection = DriverManager.getConnection(connectionUrl);
-                 PreparedStatement prepsDeleteMenuItem = connection.prepareStatement(deleteSql, Statement.RETURN_GENERATED_KEYS);) {
-
-         	prepsDeleteMenuItem.execute();
-         	System.out.println("Deleted Successfully.");
+                 Statement DeleteMenuItem = connection.createStatement();) {
+        	if(Select(name, ID)) {
+	        	String deleteSql = "DELETE FROM [dbo].[Menu_Items] WHERE " + "Name = " + "'" + name + "'" + "AND " + "ID = " + ID + ";";
+	        	while(true) {
+		        	System.out.println("Are you sure you would like to delete this entry? Y/N");
+		        	input = new Scanner(System.in);
+		        	switch(input.next()) {
+		        		case "y":
+		        		case "Y":
+		                 	DeleteMenuItem.executeUpdate(deleteSql);
+		                 	System.out.println("Deleted Successfully.");
+		        			break;
+		        		case "n":
+		        		case "N":
+		        			System.out.println("Delete Aborted.");
+		        			break;
+		        		default:
+		        			continue;
+		        			
+		        	}
+		        	break;
+	        	}
+        	}
+        	else {
+        		
+        	}
          }
          // Handle any errors that may have occurred.
          catch (Exception e) {
              e.printStackTrace();
          }
+         
     }
 
 }
